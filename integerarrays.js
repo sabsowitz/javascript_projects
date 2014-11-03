5f94d48da28840a2be68d83839412444413fd499
sabsowitz <sabsowitz@yahoo.com>
2014-11-03 16:56:17 -0500

    Integer Arrays

---
+++ integerarrays.js
@@ -0,0 +1,18 @@
+// add 2 integer arrays (add elements)
+function sumOfArrays(a, b) {
+  var result = [];
+  var minLength = a.length < b.length ? a.length : b.length;
+  for (var i=0; i<minLength; i++) {
+    result.push(a[i] + b[i]);
+  }
+  var maxLength = a.length < b.length ? b.length : a.length;
+  var longerArray = a.length < b.length ? b : a;
+  for (var i=minLength; i<maxLength; i++) {
+    result.push(longerArray[i]);
+  }
+  return result;
+}
+var a = [1, 2, 3];
+var b = [2, 3, 4, 5, 6];
+
+console.log("sum of arrays = " + sumOfArrays(a, b));
