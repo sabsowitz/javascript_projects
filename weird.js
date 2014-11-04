c310ccf43aa2de1dc9311de1398920edac993b1f
sabsowitz <sabsowitz@yahoo.com>
2014-11-04 09:36:54 -0500

    weird.js

---
+++ weird.js
@@ -0,0 +1,14 @@
+var weird = [1, 40, "bob", [], false, 89];
+
+function sum(array){
+  var sum = 0;
+  for (var i = 0; i < array.length; i++){
+    if(typeof array[i] === 'number'){
+      sum += array[i];
+    }
+  }
+  console.log(sum);
+}
+
+
+sum(weird);
