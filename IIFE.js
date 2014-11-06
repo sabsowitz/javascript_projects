4396b3127468c59f884d7399ecbb2f3919c0292e
sabsowitz <sabsowitz@yahoo.com>
2014-11-06 10:04:31 -0500

    Immediately Invoked Function Expression

--- IIFE.js
+++ IIFE.js
@@ -1,11 +1,19 @@
 // Immediately Invoked Function Expression
-var counter = (function() {
+var counter = (function() { // The variable is set to the function.
   var i = 0;
-  return {
-    get: function() { return i; },
+  return { // Beginning of the closure 
+    get: function() { return i; }, 
     set: function( val ) { i = val; },
     increment: function() {
       return ++i;
     }
   };
 }());
+
+counter.get();
+counter.set(3);
+counter.increment();
+counter.increment();
+
+// Garbage collection --> manually manage all memory. Stack heap.
+
